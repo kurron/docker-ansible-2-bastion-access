d48b39b1724f054710d1d9cef3b28cea70d65166
Ron Kurr <kurr@kurron.org>
2017-10-02 09:44:06 -0400

    Adding in code to automatically detect the Bastion server

--- test.sh
+++ test.sh
@@ -10,7 +10,29 @@ USER_ID=$(id -u $(whoami))
 GROUP_ID=$(id -g $(whoami))
 WORK_AREA=/work-area
 HOME_DIR=$(cut -d: -f6 < <(getent passwd ${USER_ID}))
-BASTION=35.163.107.71
+
+PROJECT=${1:-Weapon-X}
+ENVIRONMENT=${2:-development}
+REGION=${3:-us-west-2}
+
+STATE_FILTER=Name=instance-state-name,Values=running
+#PROJECT_FILTER=Name=tag:Project,Values=${PROJECT}
+PROJECT_FILTER=Name=tag:Project,Values=Weapon-X
+ENVIRONMENT_FILTER=Name=tag:Environment,Values=${ENVIRONMENT}
+NAME_FILTER=Name=tag:Name,Values=Bastion
+
+EC2_CMD="aws --profile qa \
+             --region ${REGION} \
+             ec2 describe-instances \
+             --filters ${STATE_FILTER} \
+             --filters ${PROJECT_FILTER} \
+             --filters ${ENVIRONMENT_FILTER} \
+             --filters ${NAME_FILTER} \
+             --query Reservations[*].Instances[*].[PublicIpAddress] \
+             --output text"
+echo ${EC2_CMD}
+BASTION=$(${EC2_CMD})
+echo ${BASTION}
 
 ADD_KEY="ssh-add bastion"
 echo ${ADD_KEY}
