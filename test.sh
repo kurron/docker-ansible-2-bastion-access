30a26dccf98b2fb395854cfb3597e16337fb6282
Ron Kurr <kurr@kurron.org>
2017-10-02 10:49:30 -0400

    Moved logic to individual functions

--- test.sh
+++ test.sh
@@ -5,55 +5,66 @@
 #   * ansible --version
 #   * ansible all --inventory='localhost,' --connection=local -m ping
 
-SSH_GROUP_ID=$(cut -d: -f3 < <(getent group ssh))
-USER_ID=$(id -u $(whoami))
-GROUP_ID=$(id -g $(whoami))
-WORK_AREA=/work-area
-HOME_DIR=$(cut -d: -f6 < <(getent passwd ${USER_ID}))
 
 PROJECT=${1:-Weapon-X}
 ENVIRONMENT=${2:-development}
 REGION=${3:-us-west-2}
 
-STATE_FILTER=Name=instance-state-name,Values=running
-#PROJECT_FILTER=Name=tag:Project,Values=${PROJECT}
-PROJECT_FILTER=Name=tag:Project,Values=Weapon-X
-ENVIRONMENT_FILTER=Name=tag:Environment,Values=${ENVIRONMENT}
-NAME_FILTER=Name=tag:Name,Values=Bastion
+function determineBastionAddress() {
+  local STATE_FILTER=Name=instance-state-name,Values=running
+  local PROJECT_FILTER=Name=tag:Project,Values=$2
+  local ENVIRONMENT_FILTER=Name=tag:Environment,Values=$3
+  local NAME_FILTER=Name=tag:Name,Values=$4
 
-EC2_CMD="aws --profile qa \
-             --region ${REGION} \
-             ec2 describe-instances \
-             --filters ${STATE_FILTER} \
-             --filters ${PROJECT_FILTER} \
-             --filters ${ENVIRONMENT_FILTER} \
-             --filters ${NAME_FILTER} \
-             --query Reservations[*].Instances[*].[PublicIpAddress] \
-             --output text"
-echo ${EC2_CMD}
-BASTION=$(${EC2_CMD})
-echo ${BASTION}
+  local EC2_CMD="aws --profile qa \
+               --region $1 \
+               ec2 describe-instances \
+               --filters ${STATE_FILTER} \
+               --filters ${PROJECT_FILTER} \
+               --filters ${ENVIRONMENT_FILTER} \
+               --filters ${NAME_FILTER} \
+               --query Reservations[*].Instances[*].[PublicIpAddress] \
+               --output text"
+  echo ${EC2_CMD}
+  BASTION=$(${EC2_CMD})
+  echo "Bastion IP address is ${BASTION}"
 
-ADD_KEY="ssh-add bastion"
-echo ${ADD_KEY}
-${ADD_KEY}
+}
 
-CMD="docker run --net host \
-                --add-host bastion:${BASTION}\
-                --hostname inside-docker \
-                --env HOME=${HOME_DIR} \
-                --env SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
-                --interactive \
-                --name deployer-test \
-                --rm \
-                --tty \
-                --user=${USER_ID}:${GROUP_ID} \
-                --volume ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} \
-                --volume $(pwd):$(pwd) \
-                --volume ${HOME_DIR}:${HOME_DIR} \
-                --volume /etc/passwd:/etc/passwd \
-                --volume /etc/group:/etc/group \
-                --workdir $(pwd) \
-                dockeransible2bastionaccess_deployer:latest $*"
-echo $CMD
-$CMD
+function addKeyToAgent() {
+  local ADD_KEY="ssh-add bastion"
+  echo ${ADD_KEY}
+  ${ADD_KEY}
+}
+
+function runContainer() {
+  local SSH_GROUP_ID=$(cut -d: -f3 < <(getent group ssh))
+  local USER_ID=$(id -u $(whoami))
+  local GROUP_ID=$(id -g $(whoami))
+  local WORK_AREA=/work-area
+  local HOME_DIR=$(cut -d: -f6 < <(getent passwd ${USER_ID}))
+
+  local CMD="docker run --net host \
+                  --add-host bastion:${BASTION} \
+                  --hostname inside-docker \
+                  --env HOME=${HOME_DIR} \
+                  --env SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
+                  --interactive \
+                  --name deployer-test \
+                  --rm \
+                  --tty \
+                  --user=${USER_ID}:${GROUP_ID} \
+                  --volume ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} \
+                  --volume $(pwd):$(pwd) \
+                  --volume ${HOME_DIR}:${HOME_DIR} \
+                  --volume /etc/passwd:/etc/passwd \
+                  --volume /etc/group:/etc/group \
+                  --workdir $(pwd) \
+                  dockeransible2bastionaccess_deployer:latest $*"
+  echo $CMD
+  $CMD
+}
+
+determineBastionAddress ${REGION} Weapon-X ${ENVIRONMENT} Bastion
+addKeyToAgent
+runContainer
